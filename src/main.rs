dac0067fc63cc8503f18afada1edc11dc181e425
FraI3mega <projeapp2@gmail.com>
2024-02-28 16:41:48 +0100

    🎨 style(main.rs): rename struct fields to snake case

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,27 @@ use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
 struct StopData {
-    stopPointSymbol: String,
-    stopPointId: u8,
-    stopPointName: String,
-    responseDate: usize,
+    stop_point_symbol: String,
+    stop_point_id: u8,
+    stop_point_name: String,
+    response_date: usize,
     departures: Vec<BusData>,
 }
 
 #[derive(Deserialize, Debug)]
 struct BusData {
-    courseId: usize,
-    scheduledDepartureSec: u32,
-    scheduledDeparture: usize,
-    realDeparture: usize,
-    vehicleId: String,
-    variantId: u32,
-    orderInCourse: u8,
+    course_id: usize,
+    scheduled_departure_sec: u32,
+    scheduled_departure: usize,
+    real_departure: usize,
+    vehicle_id: String,
+    variant_id: u32,
+    order_in_course: u8,
     passed: bool,
     lack: bool,
-    onStopPoint: bool,
-    lineName: String,
-    directionName: String,
+    on_stop_point: bool,
+    line_name: String,
+    direction_name: String,
 }
 
 fn main() -> Result<()> {
