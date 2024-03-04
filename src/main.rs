14f7c193ff579c239179c4ac6036770cf12d9aed
FraI3mega <projeapp2@gmail.com>
2024-03-04 18:40:20 +0100

    ♻️  refactor(stops): move stop logic to stops.rs

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,12 @@
+mod stops;
+
 use std::collections::HashMap;
-use std::io::Cursor;
 
 use color_eyre::eyre::Result;
 use comfy_table::Table;
-use serde::Deserialize;
-use skim::prelude::*;
 use time::OffsetDateTime;
 
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct StopData {
-    _stop_point_symbol: String,
-    _stop_point_id: u16,
-    _stop_point_name: String,
-    _response_date: usize,
-    departures: Vec<BusData>,
-}
-
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct BusData {
-    _course_id: usize,
-    _scheduled_departure_sec: u32,
-    _scheduled_departure: usize,
-    real_departure: i64, // NOTE: this in in miliseconds
-    _vehicle_id: String,
-    _variant_id: u32,
-    _order_in_course: u16,
-    _passed: bool,
-    _lack: bool,
-    _on_stop_point: bool,
-    line_name: String,
-    direction_name: String,
-}
+use crate::stops::{get_stop_data, select_stop};
 
 fn main() -> Result<()> {
     color_eyre::install()?;
@@ -56,49 +30,6 @@ fn main() -> Result<()> {
         ]);
     }
 
-    // println!(
-    //     "Stop id: {}, stop name: {}",
-    //     stop_number, stop_data.stop_point_name
-    // );
-    println!("{}", table);
+    println!("\n{}", table);
     Ok(())
-}
-
-fn get_stop_data(stop_number: u16) -> Result<StopData> {
-    let data: StopData = ureq::get(
-        format!(
-            "https://dip.mzkopole.pl/getRealtime.json?stopPointSymbol={}",
-            stop_number
-        )
-        .as_str(),
-    )
-    .call()?
-    .into_json()?;
-
-    Ok(data)
-}
-
-fn select_stop(stops: HashMap<String, u16>) -> Result<u16> {
-    let options = SkimOptionsBuilder::default()
-        .height(Some("50%"))
-        .multi(true)
-        .build()
-        .unwrap();
-
-    let input = stops
-        .keys()
-        .map(|s| s.as_str())
-        .collect::<Vec<&str>>()
-        .join("\n");
-
-    // `SkimItemReader` is a helper to turn any `BufRead` into a stream of `SkimItem`
-    // `SkimItem` was implemented for `AsRef<str>` by default
-    let item_reader = SkimItemReader::default();
-    let items = item_reader.of_bufread(Cursor::new(input));
-
-    // `run_with` would read and show items from the stream
-    let selected_items = Skim::run_with(&options, Some(items))
-        .map(|out| out.selected_items)
-        .unwrap_or_else(|| Vec::new());
-    Ok(*stops.get(&selected_items[0].output().to_string()).unwrap())
 }
