c42f75e79650b608dce96b01f98f320946dcc90f
FraI3mega <projeapp2@gmail.com>
2024-03-25 19:16:42 +0100

    💥 boom!: prepare for tui

--- src/main.rs
+++ src/main.rs
@@ -14,23 +14,6 @@ fn main() -> Result<()> {
     dbg!(sitemap.clone());
 
     let stops: HashMap<String, u16> = get_stops(sitemap.clone())?;
-    let stop_number = select_stop(stops)?;
-
-    let stop_data = get_stop_data(sitemap, stop_number)?;
-    let mut table = Table::new();
-    table.set_header(vec!["Line number", "Line name", "Arrives in"]);
-
-    for bus in stop_data.departures {
-        let departs_in = OffsetDateTime::from_unix_timestamp(bus.real_departure / 1000)?
-            - OffsetDateTime::now_utc();
-
-        table.add_row(vec![
-            bus.line_name,
-            bus.direction_name,
-            format!("{}m", departs_in.whole_minutes().to_string()),
-        ]);
-    }
-
-    println!("\n{}", table);
+    //TODO: implement ui
     Ok(())
 }
